728e0e8f8f06c88556b890e8b56b4d79c8adaba1
Julius Steuer <julius.steuer@mailbox.org>
2024-04-30 13:33:28 +0200

    remove install_requires from setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=dependencies,
+    #install_requires=dependencies,
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
