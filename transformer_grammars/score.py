80767cd5bd5b3dc36750b54248b68ba0bfaf546d
wolfingten <jagu00005@stud.uni-saarland.de>
2025-02-27 17:10:06 +0100

    cross likelihood

--- transformer_grammars/score.py
+++ transformer_grammars/score.py
@@ -128,7 +128,7 @@ def main(tokenizer, checkpoint_path, input_, output, add_eos, _):
         labels = chunk.labels[0]
         seq_log_prob += chunk_log_prob
         total_log_prob += chunk_log_prob
-        total_chars += sum([len(c.strip("▁")) for c in labels])
+        total_chars += sum([len(dic[i].strip("▁")) for i in labels])
         if chunk.beginning_of_seq.item():
             print("=" * 80)
         for inp, lab, lp, ls in zip(inputs, labels, labels_log_probs, labels_surp):
