e7be270b88d06b65b9e2441bbd4bde2a0653a530
wolfingten <jagu00005@stud.uni-saarland.de>
2025-02-28 14:43:23 +0100

    perplexity per word

--- transformer_grammars/score.py
+++ transformer_grammars/score.py
@@ -128,7 +128,7 @@ def main(tokenizer, checkpoint_path, input_, output, add_eos, _):
         labels = chunk.labels[0]
         seq_log_prob += chunk_log_prob
         total_log_prob += chunk_log_prob
-        total_tokens += len(labels)
+        total_tokens += len([l for l in labels if l.startswith("▁")])
         if chunk.beginning_of_seq.item():
             print("=" * 80)
         for inp, lab, lp, ls in zip(inputs, labels, labels_log_probs, labels_surp):
@@ -154,6 +154,7 @@ def main(tokenizer, checkpoint_path, input_, output, add_eos, _):
         writer = csv.writer(file)
         writer.writerows(rows)
     print(f"Total dataset log probability: {total_log_prob:.2f}")
-    cross_ent = -((total_log_prob / jnp.log(2)) / total_tokens)
+    log2prob = total_log_prob / jnp.log(2)
+    cross_ent = -1 * (log2prob / total_tokens)
     print(f"Cross-Entropy: {cross_ent}")
     print(f"Perplexity: {2**cross_ent}")
