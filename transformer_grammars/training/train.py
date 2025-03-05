bdd02cb7cb99e3e97c0e4670c9e2faa81f06d3a1
wolfingten <jagu00005@stud.uni-saarland.de>
2025-03-05 15:33:04 +0100

    investigate model params

--- transformer_grammars/training/train.py
+++ transformer_grammars/training/train.py
@@ -491,7 +491,7 @@ def main(config, _):
     params, state = _initialize_model(
         config.model, maskrules, token_type_ranges, init_rng, first_batch
     )
-    print(params)
+    print(f"### model params {params}")
     opt_init, _ = _optimizer(config.training.optimizer, 0.0)
     opt_state = jax.pmap(opt_init)(params)
     step = _replicate_to_local_devices(jnp.zeros((), dtype=jnp.int32))
