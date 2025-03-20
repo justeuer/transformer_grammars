0fd9de6d32ea293327487eaf445f63043e3d59a8
wolfingten <jagu00005@stud.uni-saarland.de>
2025-03-20 17:16:44 +0100

    calculate val loss only on terminals

--- transformer_grammars/training/train.py
+++ transformer_grammars/training/train.py
@@ -226,6 +226,7 @@ def _loss(apply, maskrules, vocab_size, params, state, rng, batch):
     loss = optax.softmax_cross_entropy(logits, labels_one_hot)
     total_loss = jnp.sum(mask * loss)
     total_count = jnp.sum(mask)
+    jax.debug.print("debug {}", mask[0])
     # Compute the average loss per-token for the batches received on each device
     # independently, then use that to get the per-device gradient, then average
     # those. This is fine here, as batches on each device roughly have the same
