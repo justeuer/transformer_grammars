50278ec6b821a68c7c8fc7dbbd3fd929f6f7a8a2
wolfingten <jagu00005@stud.uni-saarland.de>
2025-04-03 18:06:43 +0200

    inspect eval batches

--- transformer_grammars/training/train.py
+++ transformer_grammars/training/train.py
@@ -358,7 +358,7 @@ def _build_evaluator(eval_cfg, model_cfg, maskrules, token_type_ranges):
         total_loss = jax.lax.psum(total_loss, axis_name="i")
         total_count = jax.lax.psum(total_count, axis_name="i")
         # total_terminals = jax.lax.psum(mask, axis_name="i")
-        total_terminals = jax.sum(mask)
+        total_terminals = jnp.sum(mask)
         jax.debug.print("debug {}", total_count)
         jax.debug.print("debug {}", total_terminals)
         return state, (total_loss, total_count)
